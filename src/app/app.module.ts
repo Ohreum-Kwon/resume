766974eebc87da8a5c4a641041e5c81b0a2b93ff
iceicebaebee <44150243+iceicebaebee@users.noreply.github.com>
2021-01-05 06:38:08 -0800

    feat: add home tab (#14)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,29 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeView } from './views/home/home.view';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeView
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTabsModule,
-    MatIconModule
+    MatIconModule,
+    CommonModule,
+    FlexLayoutModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
