f6bd2e33427f7cdc1dda98f07d4460095e1ad798
Ohreum Kwon <ohreumkwon@gmail.com>
2021-01-04 16:37:47 -0800

    feat: add home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CommonModule } from '@angular/common';
+import { HomeView } from './views/home/home.view';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeView
   ],
   imports: [
     BrowserModule,
@@ -18,7 +23,9 @@ import { CommonModule } from '@angular/common';
     BrowserAnimationsModule,
     MatTabsModule,
     MatIconModule,
-    CommonModule
+    CommonModule,
+    FlexLayoutModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
