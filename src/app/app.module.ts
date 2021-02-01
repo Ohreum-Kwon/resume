12753434e367275a0273a6eccb87999516977e8d
Ohreum Kwon <ohreumkwon@gmail.com>
2021-02-01 13:42:36 -0800

    feat: expereince view (edited about and projects views)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatIconModule } from '@angular/material/icon';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatGridListModule } from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
 import { NgVerticalTimelineModule } from 'ng-vertical-timeline';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,13 +17,15 @@ import { AppComponent } from './app.component';
 import { HomeView } from './views/home/home.view';
 import { AboutView } from './views/about/about.view';
 import { ProjectsView } from './views/projects/projects.view';
+import { ExperienceView } from './views/experience/experience.view';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeView,
     AboutView,
-    ProjectsView
+    ProjectsView,
+    ExperienceView
   ],
   imports: [
     BrowserModule,
@@ -34,7 +38,9 @@ import { ProjectsView } from './views/projects/projects.view';
     MatToolbarModule,
     NgVerticalTimelineModule,
     MatGridListModule,
-    MatCardModule
+    MatCardModule,
+    MatDividerModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/experience/experience.view.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'experience-view',
+  templateUrl: './experience.view.html',
+  styleUrls: ['./experience.view.css']
+})
+export class ExperienceView implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
